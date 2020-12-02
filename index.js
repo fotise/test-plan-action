8d435b68196e79ed8ef810c7d30371c3f6137f9b
Alain Hélaïli <alain@carbonhub.io>
2020-12-02 14:05:37 +0100

    Create the project

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-try {
-  const projectName = core.getInput('name')
-  
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  core.debug(`The event payload: ${payload}`)
-  core.info(`Creating ${projectName}`)
-} catch (error) {
-  core.setFailed(error.message)
+async function run() {
+  try {
+    const projectName = core.getInput('name')
+    const ghToken = core.getInput('token')
+    const context = github.context
+    const octokit = new github.GitHub(ghToken)
+    
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    core.debug(`The event payload: ${payload}`)
+    core.info(`Creating "${projectName}" project board`)
+
+    octokit.projects.createForRepo({
+      ...context.repo,
+      projectName,
+    })
+
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
