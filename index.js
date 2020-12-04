58c0f2969d12c9bbb01b104c509489023e677023
Alain Hélaïli <alain@carbonhub.io>
2020-12-04 21:31:17 +0100

    need to be async

--- index.js
+++ index.js
@@ -4,60 +4,61 @@ const yaml = require('js-yaml')
 const fs   = require('fs')
 const fm = require('front-matter')
 
-try {
-  const config = core.getInput('config')
-  const ghToken = core.getInput('token')
-  const octokit = github.getOctokit(ghToken)
-  const context = github.context
-  
-  const configDoc = yaml.safeLoad(fs.readFileSync(config, 'utf8'));
-  core.debug(`Loaded config ${JSON.stringify(configDoc)}`)
+async function run() {
+  try {
+    const config = core.getInput('config')
+    const ghToken = core.getInput('token')
+    const octokit = github.getOctokit(ghToken)
+    const context = github.context
+    
+    const configDoc = yaml.safeLoad(fs.readFileSync(config, 'utf8'));
+    core.debug(`Loaded config ${JSON.stringify(configDoc)}`)
 
-  const projectParams = {
-      ...context.repo,
-      name: configDoc.name
-  }
-
-  core.debug(`Creating "${configDoc.name}" project board in ${projectParams.owner}/${projectParams.repo}`)
-
-  octokit.projects.createForRepo(projectParams).then(createRepoResponse => {
-    const projectId = createRepoResponse.data.id
-    let previousColumnId 
-
-    for (let index in configDoc.columns) {
-      const columnName = configDoc.columns[index]
-      core.debug(`Adding column ${columnName}`)
-
-      const column = await octokit.projects.createColumn({
-        project_id: projectId,
-        name: columnName
-      })
-
-      let postion
-      
-      if (index == 0) {
-        generateIssues(octokit, configDoc.folder, projectParams.owner, projectParams.repo, column.node_id)
-        postion = 'first'
-      } else {
-        postion = `after:${previousColumnId}`
-      }
-
-      previousColumnId  = column.id
-
-      core.debug(`Moving column ${columnName} to position ${postion}`)
-
-      octokit.projects.moveColumn({
-        column_id: column.id,
-        position: postion
-      })
+    const projectParams = {
+        ...context.repo,
+        name: configDoc.name
     }
-  }).catch(createRepoError => {
-    core.setFailed(`Failed creating the project with error: ${createRepoError.message}`)
-  })
-} catch (error) {
-  core.setFailed(error.message)
-}
 
+    core.debug(`Creating "${configDoc.name}" project board in ${projectParams.owner}/${projectParams.repo}`)
+
+    octokit.projects.createForRepo(projectParams).then(createRepoResponse => {
+      const projectId = createRepoResponse.data.id
+      let previousColumnId 
+
+      for (let index in configDoc.columns) {
+        const columnName = configDoc.columns[index]
+        core.debug(`Adding column ${columnName}`)
+
+        const column = await octokit.projects.createColumn({
+          project_id: projectId,
+          name: columnName
+        })
+
+        let postion
+        
+        if (index == 0) {
+          generateIssues(octokit, configDoc.folder, projectParams.owner, projectParams.repo, column.node_id)
+          postion = 'first'
+        } else {
+          postion = `after:${previousColumnId}`
+        }
+
+        previousColumnId  = column.id
+
+        core.debug(`Moving column ${columnName} to position ${postion}`)
+
+        octokit.projects.moveColumn({
+          column_id: column.id,
+          position: postion
+        })
+      }
+    }).catch(createRepoError => {
+      core.setFailed(`Failed creating the project with error: ${createRepoError.message}`)
+    })
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
 
 function generateIssues(octokit, folder, owner, repo, columnId) {
   var files = fs.readdirSync(folder)
@@ -113,3 +114,5 @@ function generateIssues(octokit, folder, owner, repo, columnId) {
     })
   }
 }
+
+run()
